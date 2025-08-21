4110ff55dbb5d737abaa874e7e3f0446c38e34ba
Piyumal Shalinda <piyumalbandaranayake24790@gmail.com>
2025-08-22 00:03:35 +0530

    Connect Mongooes

--- Backend/app.js
+++ Backend/app.js
@@ -1,1 +1,29 @@
-console.log('Server is running on port 3000');
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';    
+import connectDB from './src/config/DB.js';
+
+dotenv.config();
+connectDB();    
+
+const app= express();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('MongoDB Connected');       
+}).catch((error) => {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+});
+app.get('/', (req, res) => {
+    res.send('Backend is running...');
+}); 
+
+const port = process.env.Port || 5000;
+app.listen(port, () => {    
+    console.log(`Server running on port ${port}`);
+}   );
+
