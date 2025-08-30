1215824de404a91b91ae157a48cee697db91d06f
SEW123578 <sewminiwijesiri5@gmail.com>
2025-08-30 14:30:20 +0530

    feat: Add retrieval endpoints for Emergency Officers and Safari Drivers

--- Backend/src/routes/user/safariDriverroute.js
+++ Backend/src/routes/user/safariDriverroute.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { registerSafariDriver, getSafariDrivers } from '../../controllers/user/safariDrivercontroller.js';
+import { registerSafariDriver, getSafariDrivers,getSafariDriverById } from '../../controllers/user/safariDrivercontroller.js';
 
 const router = express.Router();
 // Register safari driver route
 router.post('/register', registerSafariDriver);
 // Get all safari drivers
 router.get('/', getSafariDrivers);
+// Get safari driver by ID
+router.get('/:id', getSafariDriverById);
 
 export default router;
 
