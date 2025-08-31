8c9817d3b7819394423e68788f7ba4c0fa96ab50
Piyumal Shalinda <piyumalbandaranayake24790@gmail.com>
2025-08-31 15:41:23 +0530

    refactor: Remove system login routes and clean up app.js

--- Backend/src/routes/auth/systemLoginRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { systemLogin } from '../../controllers/auth/systemLoginController.js';
-
-const router = express.Router();
-
-// POST /api/system/login
-router.post('/login', systemLogin);
-
-export default router;
